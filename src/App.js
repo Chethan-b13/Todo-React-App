b47b97355f1b3e1266ac19fd37ee19c724a8f5f2
chethancheths <chethancheths13@gmail.com>
2023-01-30 16:34:08 +0530

    ui design updated and modify feature added

--- src/App.js
+++ src/App.js
@@ -3,37 +3,59 @@ import { Input } from "./Input.js";
 import { useState } from "react";
 
 function App() {
-  const [todos, settodos] = useState([]);
+  const [todos, settodos] = useState(["Task 1", "Task 2", "Task 3"]);
   const [inputValue, setinputValue] = useState("");
 
-  const addtodo = () => {
+  const addtodo = (e) => {
     settodos(todos.concat([inputValue]));
+    setinputValue("");
   };
 
   return (
-    <div className="App">
+    <div className="Container">
       <div className="enterText">
         <input
+          className="inputArea"
+          placeholder="Enter Your Task"
           type="text"
+          value={inputValue}
           onInput={(e) => {
             setinputValue(e.target.value);
           }}
         />
-        <button onClick={addtodo}>Add</button>
+        <button type="submit" onClick={addtodo} onKeyDown={addtodo}>
+          &#x2b;
+        </button>
       </div>
 
-      <ul>
-      {todos.map((todo, index) => {
-        return (
-          <li className="list">
-            <a key={index}>{todo}</a>
-            <button className="butt" onClick={(index)=>{settodos(todos.filter(item => item !== todo));}}>Delete</button>
-            <button className="butt">Modify</button>
-          </li>
-        );
-      })}
-      </ul>
-      
+      <div className="taskArea">
+        {todos.map((todo, index) => {
+          return (
+            <div key={index} className="taskBox">
+              <p>{todo}</p>
+              <div className="buttonwrapper">
+                <button
+                  className="butt"
+                  onClick={() => {
+                    settodos(todos.filter((item) => item !== todo));
+                  }}
+                >
+                  &#128465;
+                </button>
+                <button
+                  className="butt"
+                  onClick={() => {
+                    setinputValue(todo);
+                    settodos(todos.filter((item) => item !== todo));
+                  }}
+                >
+                  &#9998;
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
